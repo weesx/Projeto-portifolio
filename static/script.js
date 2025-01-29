f1d68a8032bf17f9c538e638311ad432cc90e98d
weesx <weslei002@gmail.com>
2025-01-29 16:19:21 -0300

    portfolio e contato

--- static/script.js
+++ static/script.js
@@ -9,3 +9,11 @@ menuMobile.addEventListener('click', () =>{
     : menuMobile.classList.replace("bi-x", "bi-list");
 body.classList.toggle("menu-nav-active");
 });
+
+// Inicializa o carrossel com um intervalo de 5 segundos (5000 ms)
+var myCarousel = document.querySelector('#carouselPortfolio');
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 5000, // Tempo em milissegundos
+    wrap: true // Faz o carrossel voltar ao início após o fim
+});
+
